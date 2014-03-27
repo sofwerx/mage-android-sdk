d04aff452eb69097eecdca74e1f324de5a661856
Dan Barela <daniel.barela@gmail.com>
2014-03-27 14:06:24 -0600

    check if there is a url property first

--- src/mil/nga/giat/mage/sdk/gson/deserializer/ObservationDeserializer.java
+++ src/mil/nga/giat/mage/sdk/gson/deserializer/ObservationDeserializer.java
@@ -115,7 +115,9 @@ public class ObservationDeserializer implements JsonDeserializer<Observation> {
 				attachment.setName(jsonAttachment.get("name").getAsString());
 				attachment.setSize(jsonAttachment.get("size").getAsLong());
 				attachment.setRemoteId(jsonAttachment.get("id").getAsString());
-				attachment.setUrl(jsonAttachment.get("url").getAsString());
+				if (jsonAttachment.has("url")) {
+					attachment.setUrl(jsonAttachment.get("url").getAsString());
+				}
 				attachment.setObservation(observation);
 				attachments.add(attachment);
 			}
