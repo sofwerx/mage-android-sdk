7b4f7f93c919f36801d9c27d7639cc095bea4ee0
Travis Baumgart <travis.baumgart@gmail.com>
2014-04-16 14:58:41 -0600

    Wiring Fetch asyn tasks into the Data Fetch settings on|off toggle switch.  #3 #5 #6

--- src/mil/nga/giat/mage/sdk/fetch/ServerFetchAsyncTask.java
+++ src/mil/nga/giat/mage/sdk/fetch/ServerFetchAsyncTask.java
@@ -2,11 +2,16 @@ package mil.nga.giat.mage.sdk.fetch;
 
 import mil.nga.giat.mage.sdk.ServerAsyncTask;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public abstract class ServerFetchAsyncTask extends ServerAsyncTask {
 
+	protected final SharedPreferences sharedPreferences;
+
 	public ServerFetchAsyncTask(Context context) {
 		super(context);
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
 	}
 	
 }
