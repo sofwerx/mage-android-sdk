720cb32a48db0a677f8fb14946d866f5f8cb6b61
Dan Barela <daniel.barela@gmail.com>
2014-04-24 13:59:47 -0600

    fixed import statements

--- src/mil/nga/giat/mage/sdk/http/post/MageServerPostRequests.java
+++ src/mil/nga/giat/mage/sdk/http/post/MageServerPostRequests.java
@@ -1,12 +1,6 @@
 package mil.nga.giat.mage.sdk.http.post;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 
@@ -33,6 +27,9 @@ import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.FormBodyPart;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
